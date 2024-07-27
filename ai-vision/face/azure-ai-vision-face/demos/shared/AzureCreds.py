ced39012f59f66f4f2d55a63410df7ae3e7cf59e
bhargavk <bhargav.kagtada@gmail.com>
2024-07-26 23:23:21 -0500

    commit

--- ai-vision/face/azure-ai-vision-face/demos/shared/AzureCreds.py
+++ ai-vision/face/azure-ai-vision-face/demos/shared/AzureCreds.py
@@ -3,6 +3,6 @@
 from azure.core.credentials import AzureKeyCredential
 from azure.ai.vision.face import FaceClient
 
-endpoint = "https://eus-face-api.cognitiveservices.azure.com/"
-credential = AzureKeyCredential("172a66e425914a92b4acdc064e5d9d26")
+endpoint = "https://xyz.cognitiveservices.azure.com/"
+credential = AzureKeyCredential("abc")
 face_client = FaceClient(endpoint, credential)
